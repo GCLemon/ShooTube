c04cfd9bdfc29b326016900da1d7c8082d045c98
LemonTEA <green.c.lemon@gmail.com>
2023-03-25 12:40:35 +0900

    UI構築

--- Assets/Scripts/WaveManager.cs
+++ Assets/Scripts/WaveManager.cs
@@ -5,57 +5,68 @@ using UnityEngine;
 public class WaveManager : MonoBehaviour
 {
     // 残機
-    [SerializeField]
-    private int Life;
+    [SerializeField] private int _Life;
 
     // 得点
-    [SerializeField]
-    private ulong Score;
+    [SerializeField] private ulong _Score;
 
     // プレイヤーオブジェクト
-    public GameObject Player;
-
-    // 残機表示用のアイコンオブジェクト
-    public GameObject PlayerIconPrefab;
-    public GameObject PlayerIconContainer;
-    private List<GameObject> PlayerIcons;
+    [SerializeField] private GameObject _Player;
 
     // エネミーオブジェクト
-    public List<EnemyShip> Enemies;
+    [SerializeField] private EnemyShip _Enemy;
+
+    // 残機表示用のアイコンオブジェクト
+    [SerializeField] private GameObject _PlayerIconPrefab;
+    [SerializeField] private GameObject _PlayerIconContainer;
+    private List<GameObject> _PlayerIcons;
+
+    // 得点表示用のオブジェクト
+    [SerializeField] private GameObject _ScoreTextObject;
+    private TMPro.TextMeshPro _ScoreText;
 
     // 初期化時の処理
     private void Start()
     {
-        PlayerIcons = new();
-        for(int i = 0; i < Life; ++i)
+        _PlayerIcons = new();
+        for(int i = 0; i < _Life; ++i)
         {
-            GameObject icon = Instantiate(PlayerIconPrefab);
-            icon.transform.parent = PlayerIconContainer.transform;
+            GameObject icon = Instantiate(_PlayerIconPrefab);
+            icon.transform.parent = _PlayerIconContainer.transform;
             icon.transform.localPosition = new Vector3(100 * i + 64, 0);
-            PlayerIcons.Add(icon);
+            _PlayerIcons.Add(icon);
         }
+
+        _ScoreText = _ScoreTextObject.GetComponent<TMPro.TextMeshPro>();
+    }
+
+    // 更新時の処理
+    private void Update()
+    {
+        _ScoreText.text = _Score.ToString();
     }
 
     // エネミーがヒットした時のイベント
     private void EnemyHit(ulong score)
     {
-        Score += score;
+        _Score += score;
+
     }
 
     // プレイヤーがヒットした時のイベント
     private void PlayerHit()
     {
-        Life -= 1;
-        Destroy(PlayerIcons[Life]);
-        PlayerIcons.RemoveAt(Life);
+        _Life -= 1;
+        Destroy(_PlayerIcons[_Life]);
+        _PlayerIcons.RemoveAt(_Life);
     }
 
     // プレイヤーがエクステンドした時のイベント
     private void PlayerExtend()
     {
-        Life += 1;
-        GameObject icon = Instantiate(PlayerIconPrefab);
-        icon.transform.localPosition = new Vector3(128 * Life + 64, 0);
-        PlayerIcons.Add(icon);
+        _Life += 1;
+        GameObject icon = Instantiate(_PlayerIconPrefab);
+        icon.transform.localPosition = new Vector3(128 * _Life + 64, 0);
+        _PlayerIcons.Add(icon);
     }
 }
